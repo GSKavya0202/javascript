2faf850537fda8b8ec6c681a9f838c1de392b481
GSKavya0202 <148992350+GSKavya0202@users.noreply.github.com>
2023-10-25 15:48:02 +0000

    primitive datatypes

---
+++ javascript/datatypes.js
@@ -0,0 +1,22 @@
+//alert(3+3)
+/*alert(3+3)
+^ ReferenceError: alert is not defined
+---------------------------------
+Generally alert can be used in browser window as it already have document model but not be used in node env.
+*/
+
+let x1=123
+let x2="123"
+let x3=false
+let x4=null
+let x5=undefined
+
+console.log(typeof x1)//number
+console.log(typeof x2)//string
+console.log(typeof x3)//boolean
+console.log(typeof x4)//object
+console.log(typeof x5)//undefined
+/* 
+the type of null is an object 
+ */
+
