ab7fc212733ff4f8e2e103f8b66531966aafd67a
GSKavya0202 <148992350+GSKavya0202@users.noreply.github.com>
2023-10-26 15:29:39 +0000

    conversion to boolean

---
+++ javascript/conversion_to_boolean.js
@@ -0,0 +1,80 @@
+/*-------------
+let score=true
+console.log(score) //true if false then false
+console.log(typeof score)//boolean
+---------------*/
+/*-------------
+let score=1
+console.log(score);//1
+console.log(typeof score);//boolean
+let scoreConv=Boolean(score);
+console.log(scoreConv);//true
+console.log(typeof scoreConv);//boolean
+-------------*/
+/*-------------
+let score=0
+console.log(score);//0
+console.log(typeof score);//boolean
+let scoreConv=Boolean(score);
+console.log(scoreConv);//false
+console.log(typeof scoreConv);//boolean
+-------------*/
+
+/*-------------
+let score="1"
+console.log(score);//1
+console.log(typeof score);//string
+let scoreConv=Boolean(score);
+console.log(scoreConv);//true
+console.log(typeof scoreConv);//boolean
+-------------*/
+/*-------------
+let score="0"
+console.log(score);//0
+console.log(typeof score);//string
+let scoreConv=Boolean(score);
+console.log(scoreConv);//true
+console.log(typeof scoreConv);//boolean
+-------------*/
+
+/*-------------
+let score=""
+console.log(score);//empty row will be returned
+console.log(typeof score);//string
+let scoreConv=Boolean(score);
+console.log(scoreConv);//false
+console.log(typeof scoreConv);//boolean
+-------------*/
+
+/*-------------
+let score=null
+console.log(score);//null
+console.log(typeof score);//object
+let scoreConv=Boolean(score);
+console.log(scoreConv);//false
+console.log(typeof scoreConv);//boolean
+-------------*/
+/*-------------
+let score=undefined
+console.log(score);//undefined
+console.log(typeof score);//undefined
+let scoreConv=Boolean(score);
+console.log(scoreConv);//false
+console.log(typeof scoreConv);//boolean
+-------------*/
+/*-------------
+let score=123
+console.log(score);//123
+console.log(typeof score);//number
+let scoreConv=Boolean(score);
+console.log(scoreConv);//true
+console.log(typeof scoreConv);//boolean
+-------------*/
+/*-------------
+let score
+console.log(score);//undefined
+console.log(typeof score);//undefined
+let scoreConv=Boolean(score);
+console.log(scoreConv);//false
+console.log(typeof scoreConv);//boolean
+-------------*/
