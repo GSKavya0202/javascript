4342bcc62ce4d445c7a9e467609496c23c20ccd9
GSKavya0202 <148992350+GSKavya0202@users.noreply.github.com>
2023-10-26 15:20:16 +0000

    conversion to numbers

---
+++ javascript/conversion_to_number.js
@@ -0,0 +1,50 @@
+/*-------------
+let score=33
+console.log(score) //33
+console.log(typeof score)//number
+---------------*/
+/*-------------
+let score="33"
+console.log(score);//33
+console.log(typeof score);//string
+let scoreConv=Number(score);
+console.log(scoreConv);//33
+console.log(typeof scoreConv);//number
+-------------*/
+
+/*-------------
+let score="33abc"
+console.log(score);//33abc
+console.log(typeof score);//string
+let scoreConv=Number(score);
+console.log(scoreConv);//NaN
+console.log(typeof scoreConv);//number
+-------------*/
+
+/*-------------
+let score=true 
+console.log(score);//true
+console.log(typeof score);//boolean
+let scoreConv=Number(score);
+console.log(scoreConv);//1 if false then answer will be 0
+console.log(typeof scoreConv);//number 
+-------------*/
+
+
+/*-------------
+let score=null 
+console.log(score);//null
+console.log(typeof score);//object
+let scoreConv=Number(score);
+console.log(scoreConv);//0
+console.log(typeof scoreConv);//number 
+-------------*/
+
+/*-------------
+let score=undefined
+console.log(score);//undefined
+console.log(typeof score);//undefined
+let scoreConv=Number(score);
+console.log(scoreConv);//NaN
+console.log(typeof scoreConv);//number 
+-------------*/
